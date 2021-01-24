7a81801fe54eab30a03182d01d6c9d0b12ea85e0
AmirMohammad Kargar <a.m.kargar2000@gmail.com>
2021-01-24 13:05:52 +0330

    Add needed files.

---
+++ lib/core/utils/constans.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+//Colors
+Color blue = Color(0xFF3450A1);
+Color lightBlue = Color(0xFF237BFF);
+Color darkBlue = Color(0xFF183588);
+Color deepBlue = Color(0xFF041955);
+Color skyBlue = Color(0xFF97B4FF);
+Color white = Color(0xFFFFFFFF);
+Color pink = Color(0xFFEB06FF);
