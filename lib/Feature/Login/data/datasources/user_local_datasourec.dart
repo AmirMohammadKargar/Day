cadfa405fc5ee90b52d97e3559bbd3bcdfbede24
AmirMohammad Kargar <a.m.kargar2000@gmail.com>
2021-01-31 14:42:59 +0330

    local data source impelnation of cache data source with its tests.

--- lib/Feature/Login/data/datasources/user_local_datasourec.dart
+++ lib/Feature/Login/data/datasources/user_local_datasourec.dart
@@ -1,4 +1,6 @@
 import 'package:Day/Feature/Login/domain/entities/user.dart';
+import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 import '../models/user_model.dart';
 
@@ -8,4 +10,43 @@ abstract class UserLocalDataSource {
   Future<UserModel> getUserDB(email, password);
   Future<void> cacheUser(UserModel userToCache);
   Future<User> setUser(email, password, name, lastName);
+}
+
+class UserLocalDataSourceImp implements UserLocalDataSource {
+  final SharedPreferences sharedPreferences;
+
+  UserLocalDataSourceImp({@required this.sharedPreferences});
+
+  @override
+  Future<void> cacheUser(UserModel userToCache) {
+    // TODO: implement cacheUser
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<UserModel> getUser() async {
+    final user = sharedPreferences.getStringList("USER");
+    UserModel userModel = UserModel(
+        email: user[0], password: user[1], name: user[2], lastName: user[3]);
+    return userModel;
+  }
+
+  @override
+  Future<UserModel> getUserDB(email, password) {
+    // TODO: implement getUserDB
+    throw UnimplementedError();
+  }
+
+  @override
+  // TODO: implement isLogged
+  Future<bool> get isLogged async {
+    final result = sharedPreferences.getBool("ISLOGGED");
+    return result;
+  }
+
+  @override
+  Future<User> setUser(email, password, name, lastName) {
+    // TODO: implement setUser
+    throw UnimplementedError();
+  }
 }
---
+++ test/feature/Login/data/datasources/user_local_datasources_test.dart
@@ -0,0 +1,53 @@
+import 'package:Day/Feature/Login/data/datasources/user_local_datasourec.dart';
+import 'package:Day/Feature/Login/data/models/user_model.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mockito/mockito.dart';
+import 'package:matcher/matcher.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+class MockSharedPreferences extends Mock implements SharedPreferences {}
+
+void main() {
+  UserLocalDataSourceImp dataSource;
+  MockSharedPreferences mockSharedPreferences;
+
+  setUp(() {
+    mockSharedPreferences = MockSharedPreferences();
+    dataSource = UserLocalDataSourceImp(
+      sharedPreferences: mockSharedPreferences,
+    );
+  });
+  group('get user from cache', () {
+    final tUserModel = UserModel(
+        email: "email@email.com",
+        password: "123",
+        name: "Amir",
+        lastName: "Kargar");
+    final tUserCache = ["email@email.com", "123", "Amir", "Kargar"];
+    test(
+      'should return true for logged user',
+      () async {
+        // arrange
+        when(mockSharedPreferences.getBool(any)).thenAnswer((_) => true);
+        // act
+        final result = await dataSource.isLogged;
+        // assert
+        verify(mockSharedPreferences.getBool('ISLOGGED'));
+        expect(result, equals(true));
+      },
+    );
+    test(
+      'should return user from cache',
+      () async {
+        // arrange
+        when(mockSharedPreferences.getStringList(any))
+            .thenAnswer((_) => tUserCache);
+        // act
+        final result = await dataSource.getUser();
+        // assert
+        verify(mockSharedPreferences.getStringList('USER'));
+        expect(result, equals(tUserModel));
+      },
+    );
+  });
+}
